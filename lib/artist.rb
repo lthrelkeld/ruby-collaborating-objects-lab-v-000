3e980dbf308c857abf73d215e079bdf5b0c3ba0c
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 19:47:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name, :songs
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
@@ -9,5 +11,9 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+  end
+
+  def self.save
+    @@all << self
   end
 end
